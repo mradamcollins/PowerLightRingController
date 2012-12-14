1d7a273bf7e22434cde130b0f0d9701502822a76
Adam Collins <adamcollins@dyson.com>
2012-12-14 10:30:49 +0000

    Added first two modes with an interrupt on pin 2

--- light_ring.ino
+++ light_ring.ino
@@ -1,19 +1,64 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
+#include <avr/interrupt.h>
 
-void setup() {                
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);     
+volatile boolean stateChanged = false;
+
+int mode_1_delay = 50; //msec
+
+void setup() {     
+  // initialise intterupt for button 
+  attachInterrupt(0,flagChange, FALLING);
+    
+  // initialize the digital pins as an output.
+  for (int led = 2 ; led <=16 ; ++led)
+  {
+    pinMode(led, OUTPUT);     
+  }
+}
+
+void flagChange()
+{
+  // Flag State Changed
+  stateChanged = true;
 }
 
 void loop() {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+    
+  //Mode 1 - spinning lights 
+  stateChanged = false;
+  while(!stateChanged) {
+    
+    digitalWrite(14, HIGH); // blue lights on
+    for (int led = 2 ; (led <= 7 || stateChanged); ++ led) {
+      digitalWrite(led, HIGH);       // set the inner LEDs on
+      digitalWrite(led + 6, HIGH);   // set the outter LEDs on
+      delay(mode_1_delay);           // wait 
+      digitalWrite(led, LOW);        // set the inner LEDs off
+      digitalWrite(led + 6, LOW);    // set the outter LEDs off
+      delay(mode_1_delay);           // wait 
+    }
+  } // end Mode 1
+  
+  // Mode 2
+  stateChanged = false;
+  while(!stateChanged) {
+   int rInc = random(50);
+   int gInc = random(50);
+   int bInc = random(50); 
+   int rVal = 0;
+   int gVal = 0;
+   int bVal = 0;
+   for (int i = 1 ; i < 500; ++i) { //5secs with 10 msec delay in loop
+    rVal += rInc;
+    if (rVal > 255) rVal = 255 - rVal;
+    gVal += gInc;
+    if (gVal > 255) gVal = 255 - gVal;
+    bVal += bInc;
+    if (bVal > 255) bVal = 255 - bVal;
+    
+    analogWrite(15,rVal);
+    analogWrite(16,gVal);
+    analogWrite(14,bVal);
+    delay(10); 
+   }
+  } // end Mode 2
 }
