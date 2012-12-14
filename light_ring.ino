dc00fa81455630cae4297df63c12929c480f3c02
Adam Collins <adamcollins@dyson.com>
2012-12-14 11:38:57 +0000

    Added mode 3

--- light_ring.ino
+++ light_ring.ino
@@ -2,9 +2,16 @@
 
 volatile boolean stateChanged = false;
 
+int rPin = 15;  // R
+int gPin = 16;  // G
+int bPin = 14;  // B
+
 int mode_1_delay = 50; //msec
 
 void setup() {     
+  // Setup random 
+  randomSeed();
+  
   // initialise intterupt for button 
   attachInterrupt(0,flagChange, FALLING);
     
@@ -20,14 +27,27 @@ void flagChange()
   // Flag State Changed
   stateChanged = true;
 }
+void allOff() {
+  for (int i = 2; i <=16; ++i) {
+    digitalWrite(i,LOW);
+  }
+}
+
+void allOn() {
+  for (int i = 2; i <=16; ++i) {
+    digitalWrite(i,HIGH);
+  }
+}
 
 void loop() {
     
-  //Mode 1 - spinning lights 
+  //Mode 1 - spinning lights Blue lights on
   stateChanged = false;
+  allOff();
   while(!stateChanged) {
     
-    digitalWrite(14, HIGH); // blue lights on
+    digitalWrite(bPin, HIGH); // blue lights on
+    
     for (int led = 2 ; (led <= 7 || stateChanged); ++ led) {
       digitalWrite(led, HIGH);       // set the inner LEDs on
       digitalWrite(led + 6, HIGH);   // set the outter LEDs on
@@ -38,27 +58,44 @@ void loop() {
     }
   } // end Mode 1
   
-  // Mode 2
+  // Mode 2 - random RGB values
   stateChanged = false;
+  allOff();
   while(!stateChanged) {
+   
    int rInc = random(50);
    int gInc = random(50);
    int bInc = random(50); 
    int rVal = 0;
    int gVal = 0;
    int bVal = 0;
-   for (int i = 1 ; i < 500; ++i) { //5secs with 10 msec delay in loop
+   int innerPin = random(6) + 2;
+   int outterPin = random(6) + 8;
+   digitalWrite(innerPin,HIGH);
+   digitalWrite(outterPin,HIGH);
+   for (int i = 1 ; (i < 500 || stateChanged); ++i) { //5secs with 10 msec delay in loop
     rVal += rInc;
-    if (rVal > 255) rVal = 255 - rVal;
+    if (rVal > 255) rVal = rVal - 255;
     gVal += gInc;
-    if (gVal > 255) gVal = 255 - gVal;
+    if (gVal > 255) gVal = gVal - 255;
     bVal += bInc;
-    if (bVal > 255) bVal = 255 - bVal;
+    if (bVal > 255) bVal = bVal - 255;
     
-    analogWrite(15,rVal);
-    analogWrite(16,gVal);
-    analogWrite(14,bVal);
+    analogWrite(rPin,rVal);
+    analogWrite(gPin,gVal);
+    analogWrite(bPin,bVal);
     delay(10); 
    }
+   digitalWrite(innerPin,LOW);
+   digitalWrite(outterPin,LOW);
   } // end Mode 2
+  
+  // Mode 3 - All on
+  stateChanged = false;
+  allOn();
+  while(!stateChanged) {
+      // Wait
+  }
+  
+  
 }
